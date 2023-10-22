bb8e1406d8756264f8710a6e9d344708253a031c
Siddhant <siddhantpawar572@gmail.com>
2023-10-22 19:32:09 +0530

    API Gateway application for All our microservices

---
+++ API-Gateway/src/test/java/com/siddhant/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhant.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
